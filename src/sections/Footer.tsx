176aa9cbe6b1cf7681bfc4d16ca5b06a73a8e9d3
Vaughn Antonio <vaughnantonio2001@gmail.com>
2024-09-12 19:14:45 +0400

    Major Redesign, color changes, shapes changes

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   return (
     <footer className='bg-black text-[#BCBCBC] text-sm py-10 text-center'>
       <div className="container">
-        <div className="inline-flex relative before:content-[''] before:top-2  before:bottom-2 before:w-full  before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+        <div className="inline-flex relative">
             <Image src={logo} alt='logo image' height={40} className='relative'/>  
         </div>
         
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -17,15 +17,15 @@ export const Hero = () => {
   const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
 
   return (
-  <section ref={heroRef} className="pt-8 pb-20 md:pd-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+  <section ref={heroRef} className="pt-8 pb-20 md:pd-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#2A8C82,#EAEEFE_100%)] overflow-x-clip">
     <div className="container">
       <div className="md:flex items-center">
         <div className="md:w-[478px]">
           <div className="tag">Version 1.0 is here</div>
-          <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6 lg:left-0">
+          <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-[#275950] to-[#2A8C82] text-transparent bg-clip-text mt-6 lg:left-0">
             Nexsys Platform
           </h1>
-          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+          <p className="text-xl text-[#260101] tracking-tight mt-6">
             Streamline your business with our all-in-one platform—powerful tools, 
             seamless integrations, and real-time insights to boost productivity and drive growth.
           </p>
@@ -54,8 +54,8 @@ export const Hero = () => {
           />
           <motion.img 
           src={cylinderImage.src} 
-          width={220} 
-          height={220} 
+          width={230} 
+          height={230} 
           alt="cylinder img"
           className="hidden md:block -top-8 -left-32 md:absolute"
           style={{
@@ -68,7 +68,7 @@ export const Hero = () => {
           alt="Noodle Image"
           className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
           style={{
-            rotate: 30,
+            rotate: 5,
             translateY: translateY,
           }}
           />
